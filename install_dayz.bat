a571e3eae483227c021229340109ccfdfd6ed885
Shannon Haworth <shannon.haworth@gmail.com>
2023-07-23 19:49:44 -0400

    Delete install_dayz.bat

--- install_dayz.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "steamcmdpath=C:\Users\shaworth\steamcmd"
-set "serverpath=C:\Users\shaworth\Documents\dayz\server1"
-set "login=REDACTED_CHANGE_ME" 
-echo.
-%steamcmdpath%\steamcmd +force_install_dir "%serverpath%" +login %login% %pass% +app_update 223350 validate +quit
-
