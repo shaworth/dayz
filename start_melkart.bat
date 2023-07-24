8de87090d88d3953a11c70270879f1c282d3f362
Shannon Haworth <shannon.haworth@gmail.com>
2023-07-24 02:18:31 -0400

    Update start_melkart.bat
    Fixed mod name

--- start_melkart.bat
+++ start_melkart.bat
@@ -2,6 +2,6 @@
 
 cd %UserProfile%\Documents\dayz\server1
 
-start "DayZ Server" DayZServer_x64.exe --dologs -netlog -freezecheck -adminLog -profiles=melkartprofile -config=serverDZ.cfg -port=2302 -cpuCount=6 -mod="@Melkart_beta"
+start "DayZ Server" DayZServer_x64.exe --dologs -netlog -freezecheck -adminLog -profiles=melkartprofile -config=serverDZ.cfg -port=2302 -cpuCount=6 -mod="@Melkart_Beta"
 
 cd %UserProfile%\Documents\dayz
